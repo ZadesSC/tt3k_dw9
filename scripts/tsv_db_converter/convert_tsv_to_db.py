22a93322456188c25abc5830b42c1709fed23a42
ZadesSC <zadessc@gmail.com>
2019-07-11 21:16:01 -0700

    Added loc file conversion

--- scripts/tsv_db_converter/convert_tsv_to_db.py
+++ scripts/tsv_db_converter/convert_tsv_to_db.py
@@ -10,25 +10,74 @@ schema_file_name="schema_3k.json"
 output_location="output/"
 
 def main():
-    #get schema data
+    #figure out if we are writing to a normal db or a loc
+    if args.loc:
+        write_loc()
+    else:
+        write_db()
+
+def write_loc():
+    tsv_reader = csv.reader(args.input, delimiter='\t')
+    loc_bytes = bytearray(b'')
+
+    rows_count = 0;
+    for index, row in enumerate(tsv_reader):
+        if row[0] is None:
+            print("WARN: " + args.input.name + " is missing data in row " + row + ", skipping...")
+            continue
+
+        #add utf16 key
+        str_len = len(row[0])
+        loc_bytes.extend(struct.pack("h", str_len))
+        loc_bytes.extend(bytes(row[0], 'utf16')[2:])
+
+        #add utf16 value
+        str_len = len(row[1])
+        loc_bytes.extend(struct.pack("h", str_len))
+        loc_bytes.extend(bytes(row[1], 'utf16')[2:])
+
+        #add bool
+        loc_bytes.extend(struct.pack("?", row[2]))
+
+        rows_count +=1
+
+    header_bytes = bytearray(b'')
+    #write loc header
+    header_bytes.extend(bytearray(b'\xff\xfeLOC\x00'))
+    #write loc version (always 1)
+    header_bytes.extend(bytearray(b'\x01\x00\x00\x00'))
+    #write table entries count
+    header_bytes.extend(struct.pack("i", rows_count))
+
+    total_bytes = bytearray(b'')
+    total_bytes.extend(header_bytes)
+    total_bytes.extend(loc_bytes)
+    args.output.write(total_bytes)
+
+    #close at end
+    args.input.close()
+
+
+def write_db():
+    # get schema data
     schema_data = get_schema()
 
-    #need to get a normal reader to get the header row so I can make a dict reader :thinking:
+    # need to get a normal reader to get the header row so I can make a dict reader :thinking:
     tsv_reader_header = csv.reader(args.input, delimiter='\t')
     tsv_header = list(tsv_reader_header)[2]
     args.input.close()
     f = open(args.input.name)
     p_out("Table Header: " + str(tsv_header))
 
-    #get tsv
-    #tsv_reader = csv.DictReader(f, fieldnames=tsv_header, delimiter='\t')
+    # get tsv
+    # tsv_reader = csv.DictReader(f, fieldnames=tsv_header, delimiter='\t')
     tsv_reader = csv.DictReader(f, fieldnames=list(tsv_header), delimiter='\t')
     db_bytes = bytearray(b'')
 
     row_count = 0
     for index, row in enumerate(tsv_reader):
         p_out(row)
-        #first row is table name
+        # first row is table name
         if index == 0:
             if list(row.items())[0] is None:
                 p_out("ERROR: Invalid File header, line 1: " + args.input.name)
@@ -36,30 +85,30 @@ def main():
             table_name = list(row.items())[0][1]
             p_out("Table Name: " + table_name)
             table_schema = get_table_schema(table_name, schema_data)
-        #second row is table version
+        # second row is table version
         elif index == 1:
             if list(row.items())[0] is None:
                 print("ERROR: Invalid File header, line 2: " + args.input.name)
                 exit(1)
-            table_version =  list(row.items())[0][1]
+            table_version = list(row.items())[0][1]
             p_out("Table Version: " + table_version)
-        #third row is definition
+        # third row is definition
         elif index == 2:
             pass
-        #fourth row and on is data
+        # fourth row and on is data
         else:
             if len(row) == 0:
                 continue
             db_bytes.extend(convert_row_to_binary(row, table_schema, table_version))
             row_count += 1
 
-    #write to output file
+    # write to output file
     total_bytes = bytearray(b'')
-    total_bytes.extend(convert_file_header_to_binary(table_version, row_count   ))
+    total_bytes.extend(convert_file_header_to_binary(table_version, row_count))
     total_bytes.extend(db_bytes)
     args.output.write(total_bytes)
 
-    #close file at end
+    # close file at end
     args.input.close()
     args.output.close()
 
@@ -145,6 +194,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Converts tsv files into db files for Total War: THREE KINGDOMS')
     parser.add_argument("--input", required=True, help="the input tsv file to be processed",type=argparse.FileType('r'))
     parser.add_argument("--output", required=True, help="the output db file", type=argparse.FileType('wb'))
+    parser.add_argument("--loc", help="this is a loc database",  action='store_true')
     parser.add_argument("--verbose", help="enable debug logging",  action='store_true')
     args = parser.parse_args()
     main()
